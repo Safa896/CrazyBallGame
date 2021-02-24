e7c243ae56eaf227373b3d1fe1363b6347f65772
Safa <safabouchrika33@gmail.com>
2021-02-24 13:28:27 +0100

    First commit

---
+++ Assets/Movetonextlevel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movetonextlevel : MonoBehaviour {
+ public void ChangeScene(string sceneName)
+ {
+  Application.LoadLevel(sceneName);
+ }
+}
